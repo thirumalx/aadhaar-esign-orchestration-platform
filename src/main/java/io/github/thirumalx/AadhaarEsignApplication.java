eb1ac1e226d9462322ee3a1c9637ec6450d6d8f3
Thirumal M <racetortoise@gmail.com>
2026-06-17 15:00:43 +0530

    first commit

---
+++ src/main/java/io/github/thirumalx/AadhaarEsignApplication.java
@@ -0,0 +1,13 @@
+package io.github.thirumalx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadhaarEsignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadhaarEsignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/thirumalx/AadhaarEsignApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.thirumalx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadhaarEsignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
